861bd78622d044fcadd4eaf9f9f53b85f80a41c5
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-27 00:13:47 +0300

    "Updated AppUserService and appUserRepository"

--- user-registration-login/src/main/java/com/example/userregistrationlogin/repository/AppUserRepository.java
+++ user-registration-login/src/main/java/com/example/userregistrationlogin/repository/AppUserRepository.java
@@ -2,6 +2,14 @@ package com.example.userregistrationlogin.repository;
 
 import com.example.userregistrationlogin.appuser.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly= true)
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByEmail(String email);
 }
--- user-registration-login/src/main/java/com/example/userregistrationlogin/service/AppUserService.java
+++ user-registration-login/src/main/java/com/example/userregistrationlogin/service/AppUserService.java
@@ -1,12 +1,29 @@
 package com.example.userregistrationlogin.service;
 
+import com.example.userregistrationlogin.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+    private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+
+   /* @Autowired
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }*/
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
