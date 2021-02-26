75665283c2f99f45f2a08a6bd236e72beb3af4be
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-27 01:56:27 +0300

    "Added the RequestingMapping annotation to RegistrationController"

--- user-registration-login/src/main/java/com/example/userregistrationlogin/controller/RegistrationController.java
+++ user-registration-login/src/main/java/com/example/userregistrationlogin/controller/RegistrationController.java
@@ -3,17 +3,19 @@ package com.example.userregistrationlogin.controller;
 import com.example.userregistrationlogin.appuser.RegistrationRequest;
 import com.example.userregistrationlogin.service.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/app/v1/registration")
+@RequestMapping(path = "/api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
 
     private final RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
